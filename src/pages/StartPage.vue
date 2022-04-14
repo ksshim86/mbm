37d499f21de894f49c056de71664c7d417d988fc
ksshim86 <ksshim86@gmail.com>
2022-04-14 12:53:35 +0900

    # update startpage

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -5,19 +5,40 @@
       </div>
       <div class="col-8 preview-col">
         <div class="text-h4">
-          <q-bar id="bar" class="text-white non-selectable no-pointer-events">
-            <div><q-icon name="preview" size="24px" class="q-mr-sm" color="orange"/>Preview</div>
+          <q-bar
+            id="bar"
+            class="text-white non-selectable no-pointer-events"
+          >
+            <div>
+              <q-icon
+                name="preview"
+                size="24px"
+                class="q-mr-sm"
+                color="orange"
+              />Preview</div>
             <q-space />
-            <q-btn dense flat icon="minimize" />
-            <q-btn dense flat icon="crop_square" />
-            <q-btn dense flat icon="close" />
+            <q-btn
+              dense
+              flat
+              icon="minimize"
+            />
+            <q-btn
+              dense
+              flat
+              icon="crop_square"
+            />
+            <q-btn
+              dense
+              flat
+              icon="close"
+            />
           </q-bar>
         </div>
       </div>
       <div class="col-2">
       </div>
     </div>
-    <div class="row text-center q-mt-lg">
+    <div class="row q-mt-lg">
       <div class="col-2">
       </div>
       <div class="col-8">
@@ -26,32 +47,56 @@
         </q-badge>
         <q-slider
           v-model="gridCount"
-          label-always
           markers
           marker-labels
           :min="1"
           :max="3"
           color="orange"
         >
-          <template v-slot:marker-label-group="scope">
-            <div
-              v-for="marker in scope.markerList"
-              :key="marker.index"
-              :class="[ `text-deep-orange-${2 + Math.ceil(marker.value / 2) }`, marker.classes ]"
-              :style="marker.style"
-              @click="model = marker.value"
-            >{{ marker.value }}</div>
-          </template>
+        </q-slider>
+        <q-badge color="secondary">
+          Row Count: {{ rowCount }}
+        </q-badge>
+        <q-slider
+          v-model="rowCount"
+          markers
+          marker-labels
+          :min="1"
+          :max="3"
+          color="orange"
+        >
+        </q-slider>
+        <q-badge color="secondary">
+          Col Count: {{ colCount }}
+        </q-badge>
+        <q-slider
+          v-model="colCount"
+          markers
+          marker-labels
+          :min="1"
+          :max="3"
+          color="orange"
+        >
         </q-slider>
       </div>
       <div class="col-2">
       </div>
     </div>
     <q-footer>
-        <q-btn-group spread>
-          <q-btn color="dark" text-color="orange" label="Back" icon="arrow_back" />
-          <q-btn color="dark" text-color="orange" label="Done" icon="done" />
-        </q-btn-group>
+      <q-btn-group spread>
+        <q-btn
+          color="dark"
+          text-color="orange"
+          label="Back"
+          icon="arrow_back"
+        />
+        <q-btn
+          color="dark"
+          text-color="orange"
+          label="Done"
+          icon="done"
+        />
+      </q-btn-group>
     </q-footer>
   </q-page>
 </template>
@@ -61,23 +106,27 @@ import { ref, defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'StartPage',
-  setup() {
+  setup () {
     const gridCount = ref(1)
+    const rowCount = ref(1)
+    const colCount = ref(1)
 
     return {
       gridCount,
+      rowCount,
+      colCount,
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-  #bar {
-    background-color: #202225;
-  }
+#bar {
+  background-color: #202225;
+}
 
-  .preview-col {
-    border: 1px solid;
-    height: calc(100vh - 40vh);
-  }
+.preview-col {
+  border: 1px solid;
+  height: calc(100vh - 50vh);
+}
 </style>
