a7e7d993548fcc4a3dbb27dd192428d4781d550d
ksshim86 <ksshim86@gmail.com>
2022-05-09 19:00:59 +0900

    # 수정기능 구현중

--- src/pages/BookmarkPage.vue
+++ src/pages/BookmarkPage.vue
@@ -10,17 +10,27 @@
           binary-state-sort
           :filter="filter"
           virtual-scroll
-          v-model:pagination="pagination"
+          v-model="pagination"
           :rows-per-page-options="[0]"
           class="my-sticky-header-table"
-          style="height: 500px; width: 500px;"
+          style="height: 480px; width: 500px;"
           color="orange"
         >
           <template v-slot:top>
             <q-space />
-            <q-input label="Search" borderless dense debounce="300" color="orange" v-model="filter">
+            <q-input
+              label="Search"
+              borderless
+              dense
+              debounce="300"
+              color="orange"
+              v-model="filter"
+            >
               <template v-slot:prepend>
-                <q-icon name="search" color="orange" />
+                <q-icon
+                  name="search"
+                  color="orange"
+                />
               </template>
             </q-input>
           </template>
@@ -38,20 +48,56 @@
           </template>
           <template v-slot:body="props">
             <q-tr :props="props">
-              <q-td key="name" :props="props">
+              <q-td
+                key="name"
+                :props="props"
+              >
                 {{ props.row.name }}
-                <q-popup-edit v-model="props.row.name" v-slot="scope" title="Update name" buttons color="orange">
-                  <q-input v-model="scope.value" dense autofocus color="orange" />
+                <q-popup-edit
+                  v-model="props.row.name"
+                  v-slot="scope"
+                  title="Update name"
+                  buttons
+                  color="orange"
+                  @save="editingName(props)"
+                >
+                  <q-input
+                    v-model="scope.value"
+                    dense
+                    autofocus
+                    color="orange"
+                  />
                 </q-popup-edit>
               </q-td>
-              <q-td key="url" :props="props">
+              <q-td
+                key="url"
+                :props="props"
+              >
                 {{ props.row.url }}
-                <q-popup-edit v-model="props.row.url" v-slot="scope" title="Update url" buttons color="orange">
-                  <q-input v-model="scope.value" dense autofocus color="orange" />
+                <q-popup-edit
+                  v-model="props.row.url"
+                  v-slot="scope"
+                  title="Update url"
+                  buttons
+                  color="orange"
+                  @save="editingUrl(props)"
+                >
+                  <q-input
+                    v-model="scope.value"
+                    dense
+                    autofocus
+                    color="orange"
+                  />
                 </q-popup-edit>
               </q-td>
               <q-td auto-width>
-                <q-btn size="sm" color="orange" round @click="props.expand = !props.expand" icon="delete" />
+                <q-btn
+                  size="sm"
+                  color="orange"
+                  round
+                  @click="props.expand = !props.expand"
+                  icon="delete"
+                />
               </q-td>
             </q-tr>
           </template>
@@ -73,13 +119,13 @@
 </template>
 
 <script>
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, watch } from 'vue'
 import { useMonitoringStore } from 'stores/monitoring'
 import { useRouter } from 'vue-router'
 
 const columns = [
-  {name: 'name', label: 'Name', align: 'left', field: 'name', sortable: true},
-  {name: 'url', label: 'Url', align: 'left', field: 'url', sortable: true},
+  { name: 'name', label: 'Name', align: 'left', field: 'name', sortable: true },
+  { name: 'url', label: 'Url', align: 'left', field: 'url', sortable: true },
 ]
 
 export default defineComponent({
@@ -89,9 +135,25 @@ export default defineComponent({
     const store = useMonitoringStore()
     const bookmarks = ref([])
     const filter = ref('')
+    let itemKey = -1
+
+    watch(bookmarks, (to) => {
+      if (itemKey > -1) {
+        // update 치면 된다.
+        console.log(to[itemKey - 1])
+      }
+    }, { deep: true })
 
     const handleBackBtnClicked = () => {
       router.push('/')
+    }
+
+    const editingName = (item) => {
+      itemKey = item.key
+    }
+
+    const editingUrl = (item) => {
+      itemKey = item.key
     }
 
     return {
@@ -105,12 +167,14 @@ export default defineComponent({
         await store.fetchBookmarks()
         bookmarks.value = store.bookmarks
         for (let i = 0; i < 1000; i++) {
-          bookmarks.value.push({id: i + 3, name: `foo${i + 3}`, url: `http://foo${i + 3}.com`})
+          bookmarks.value.push({ id: i + 3, name: `foo${i + 3}`, url: `http://foo${i + 3}.com` })
         }
 
-        bookmarks.value.push({id: '', name: '', url: ''})
+        bookmarks.value.push({ id: '', name: '', url: '' })
       },
       handleBackBtnClicked,
+      editingName,
+      editingUrl,
     }
   },
   created () {
@@ -121,13 +185,11 @@ export default defineComponent({
 
 <style lang="sass">
 .my-sticky-header-table
-  /* height or max-height is important */
   height: 310px
 
   .q-table__top,
   .q-table__bottom,
   thead tr:first-child th
-    /* bg color is important for th; just specify one */
     background-color: #36393f
     color: #ffbc5a
 
@@ -137,8 +199,6 @@ export default defineComponent({
   thead tr:first-child th
     top: 0
 
-  /* this is when the loading indicator appears */
   &.q-table--loading thead tr:last-child th
-    /* height of all previous header rows */
     top: 48px
 </style>
